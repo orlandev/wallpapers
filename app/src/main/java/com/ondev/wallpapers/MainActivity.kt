0a50c6b8268fdcc49dbdd8e3faa42ea15056ce5c
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-03-30 18:45:15 -0400

    ADD Bottom controllers

--- app/src/main/java/com/ondev/wallpapers/MainActivity.kt
+++ app/src/main/java/com/ondev/wallpapers/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ondev.wallpapers
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
+
     }
 }
--- app/src/main/java/com/ondev/wallpapers/WallpaperAdapter.kt
+++ app/src/main/java/com/ondev/wallpapers/WallpaperAdapter.kt
@@ -1,7 +1,6 @@
 package com.ondev.wallpapers
 
 import android.net.Uri
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,9 +16,6 @@ class WallpaperAdapter(
 ) : RecyclerView.Adapter<WallpaperAdapter.WallpaperViewHolder>() {
     inner class WallpaperViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageWallpaper: ImageView = itemView.findViewById(R.id.image_view_parallax_effect)
-        // val txtSplashTitle: TextView = itemView.findViewById(R.id.text_onboarding_title)
-        // val txtSplashDescription: TextView = itemView.findViewById(R.id.text_onboarding_description)
-
     }
 
     override fun onCreateViewHolder(
@@ -35,17 +31,10 @@ class WallpaperAdapter(
 
     override fun onBindViewHolder(holder: WallpaperViewHolder, position: Int) {
         val currentWallpaperItem = wallpaperItems[position]
-        Log.d("CURRENT_WALLPAPER", currentWallpaperItem.WallpaperFileName)
-        var tmp:String="$ASSETS_FOLDER${currentWallpaperItem.WallpaperFileName}"
-        Log.d("TTTTTTTT",tmp)
         Glide.with(holder.imageWallpaper.context)
-            .load(Uri.parse(tmp))
+            .load(Uri.parse("$ASSETS_FOLDER${currentWallpaperItem.WallpaperFileName}"))
             .transition(DrawableTransitionOptions.withCrossFade())
-            .error(R.drawable.glide_error)
             .into(holder.imageWallpaper)
-
-        //holder.txtSplashTitle.setText(currentOnBoardingItem.onBoardingTitle)
-        // holder.txtSplashDescription.setText(currentOnBoardingItem.onBoardingDescription)
     }
 
     override fun getItemCount(): Int {
